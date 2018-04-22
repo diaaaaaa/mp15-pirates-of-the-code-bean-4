c03e7667eed0bbc0b82f021b8dd62b0b255a9325
diaaaaaa <ite.diaa@gmail.com>
2018-04-22 09:02:10 +0200

    start spring

---
+++ src/main/java/at/refugeescode/mp15piratesofthecodebean4/Mp15PiratesOfTheCodeBean4Application.java
@@ -0,0 +1,12 @@
+package at.refugeescode.mp15piratesofthecodebean4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp15PiratesOfTheCodeBean4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp15PiratesOfTheCodeBean4Application.class, args);
+	}
+}
---
+++ src/test/java/at/refugeescode/mp15piratesofthecodebean4/Mp15PiratesOfTheCodeBean4ApplicationTests.java
@@ -0,0 +1,16 @@
+package at.refugeescode.mp15piratesofthecodebean4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Mp15PiratesOfTheCodeBean4ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
